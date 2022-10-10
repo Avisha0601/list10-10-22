7dbf293658170d7f1304ff2e90f8dcb46c7857de
Avisha0601 <86399682+Avisha0601@users.noreply.github.com>
2022-10-10 20:40:16 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,25 @@
+students = ["Avisha", "Gurmeher", "Myra", "Zenia"]
+print(students)
+students.append("Shivangi")
+print(students)
+
+students.remove("Shivangi")
+print(students)
+
+fruits = ["Mango", "Banana", "Apple", "orange"]
+print(fruits)
+fruits.append("kiwi")
+print(fruits)
+
+fruits.remove("Apple")
+print(fruits)
+
+total = 0
+
+list1 = [11, 5, 7, 22, 18]
+
+     for ele in range(0, len(List1)):
+    total = total + list1[ele]
+    
+print("Sum of all elements in given list: ", total)
+
